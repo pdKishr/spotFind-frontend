3a4823b22cd1e1ddfe44078427ead756e0c79323
pdKishr <pdkishr@gmail.com>
2025-02-27 18:26:25 +0530

    latest 2.3

--- src/AXIOS_INSTANCE/api_instance.ts
+++ src/AXIOS_INSTANCE/api_instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL : 'https://spotfind-backend-production.up.railway.app/',
+    baseURL : 'https://spotfind-backend-production.up.railway.app',
     timeout :  10000,
     headers :  {
         'Content-Type' : 'application/json',
