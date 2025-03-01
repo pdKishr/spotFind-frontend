2a3a14365285c80be1d853fa69b304cca79db8f8
pdKishr <pdkishr@gmail.com>
2025-03-01 22:27:15 +0530

    rountes testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwind from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwind()],
-  server: {
-    port: 5173,
-    open: true,
-    strictPort: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'`, // Ensures React is always available
-  },
-});
+
+  plugins: [
+    tailwindcss()
+  ]
+})
